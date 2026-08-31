93a3091fe030fbbbfdd993fc4d35cdfcc4862d27
mohammad <ghavidelheidarei81@gmail.com>
2026-08-31 18:16:52 +0330

    Remove android/ from main - Android development stays on feature/android-scaffold

--- android/app/src/main/java/ir/codecrafter/plasticproducts/PlasticProductsApplication.kt
+++
@@ -1,7 +0,0 @@
-package ir.codecrafter.plasticproducts
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PlasticProductsApplication : Application()
